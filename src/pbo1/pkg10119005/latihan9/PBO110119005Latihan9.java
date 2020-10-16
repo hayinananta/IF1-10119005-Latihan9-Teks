eb24346caeea452f9a9450e7aacdf1354195d2fa
hayinanjay <72958200+hayinanjay@users.noreply.github.com>
2020-10-16 11:14:35 +0700

    Add files via upload

---
+++ src/pbo1/pkg10119005/latihan9/PBO110119005Latihan9.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10119005.latihan9;
+
+/**
+ *
+ * @author Hayin
+ * NAMA     : Hayin Ananta
+ * KELAS    : IF-1
+ * NIM      : 10119005
+ * Deskripsi Program : Memunculkan Teks
+ */
+public class PBO110119005Latihan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "char: " + tab + " berisi " + c);
+        
+    }
+    
+}
